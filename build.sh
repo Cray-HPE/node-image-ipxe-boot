5b8b57687264233a25b5deef666ae668a939ba61
Patrick Force <pforce@cray.com>
2019-12-23 19:34:41 -0700

    parameterize build script and packer

--- build.sh
+++ build.sh
@@ -4,16 +4,26 @@
 # vshasta-cray project to create an iPXE Boot Disk image suitable for
 # use creating Virtual Shasta Network Booted Nodes.
 
-project_id="vshasta-cray"
-zone="us-central1-a"
+project_id=$NODE_IMAGES_BUILDER_DESTINATION_IMAGE_PROJECT_ID
+subnetwork=$NODE_IMAGES_BUILDER_SUBNETWORK
+zone=$NODE_IMAGES_BUILDER_ZONE
+
+if [ -z "$project_id" ]; then
+  read -p "What's the destination project for this build? " project_id
+fi
+if [ -z "$subnetwork" ]; then
+  read -p "What destination project subnetwork would you like to use for this build? " subnetwork
+fi
+if [ -z "$zone" ]; then
+  read -p "What destination project zone would you like to use for this build (should match the subnetwork provided)? " zone
+fi
 
 echo "Creating the ipxe-builder VM Instance"
 gcloud compute --project=$project_id instances create ipxe-builder\
        --quiet \
        --zone=$zone \
        --machine-type=n1-standard-8 \
-       --subnet=default-network-us-central1 \
-       --service-account=1015742806632-compute@developer.gserviceaccount.com \
+       --subnet=$subnetwork \
        --scopes=https://www.googleapis.com/auth/cloud-platform \
        --image-project=debian-cloud \
        --image-family=debian-9 \
